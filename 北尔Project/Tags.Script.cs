2331f3e5b1b5ac33c59de61b3a469e4ec9ebbd30
songwei <11720846@qq.com>
2020-05-27 16:19:07 +0800

    一

---
+++ 北尔Project/Tags.Script.cs
@@ -0,0 +1,39 @@
+//--------------------------------------------------------------
+// Press F1 to get help about using script.
+// To access an object that is not located in the current class, start the call with Globals.
+// When using events and timers be cautious not to generate memoryleaks,
+// please see the help for more information.
+//---------------------------------------------------------------
+
+namespace Neo.ApplicationFramework.Generated
+{
+    using System.Windows.Forms;
+    using System;
+    using System.Drawing;
+    using Neo.ApplicationFramework.Tools;
+    using Neo.ApplicationFramework.Common.Graphics.Logic;
+    using Neo.ApplicationFramework.Controls;
+    using Neo.ApplicationFramework.Interfaces;
+    
+    
+    public partial class Tags
+    {
+		
+		void CIN_AccessDenied(System.Object sender, System.EventArgs e)
+		{
+			
+		}
+		
+		void Tags_ServerInfo(System.Object sender, Neo.ApplicationFramework.Interfaces.Events.ServerInfoArgs e)
+		{
+			
+		}
+		
+		void RS_AccessDenied(System.Object sender, System.EventArgs e)
+		{
+			
+		}
+		
+	
+    }
+}
